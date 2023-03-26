a5c655640dc4d21c8e60aabb0313e853ec627e69
RK Shamim <128965318+RKSHAMIM94@users.noreply.github.com>
2023-03-26 15:34:05 +0600

    Add files via upload

---
+++ psd to html/js/main.js
@@ -0,0 +1,8 @@
+// Bx slider
+$(document).ready(function(){
+    $('.slider_area').bxSlider();
+    $('#main_menu').meanmenu({
+      meanMenucontainer: '#mobile_menu',
+      meanScreenWidth: 768,
+    });
+  });
